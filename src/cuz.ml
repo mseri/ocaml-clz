5420bfef71c2ffb439147169d5754578a1bb551b
Marcello Seri <marcello.seri@gmail.com>
2021-06-29 11:08:10 +0200

    Big update
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>

--- src/cuz.ml
+++ src/cuz.ml
@@ -1,46 +1,12 @@
-(*
-  Mostly from deflate.gz documentation :) 
-  If somebody cares, we could abstract them over Cohttp and Lwt
-  and have an implementation that can work with streamed response bodies
-  and does directly output to file.
-  The current dumb implementation is more than enough for my limited needs.
-*)
+open Cuz_decompress
 
-let uncompress_string str =
-  let i = De.bigstring_create De.io_buffer_size in
-  let o = De.bigstring_create De.io_buffer_size in
-  let r = Buffer.create 0x1000 in
-  let p = ref 0 in
-  let refill buf =
-    let len = min (String.length str - !p) De.io_buffer_size in
-    Bigstringaf.blit_from_string str ~src_off:!p buf ~dst_off:0 ~len;
-    p := !p + len;
-    len
-  in
-  let flush buf len =
-    let str = Bigstringaf.substring buf ~off:0 ~len in
-    Buffer.add_string r str
-  in
-  Gz.Higher.uncompress ~refill ~flush i o
-  |> Result.map (fun _metadata -> Buffer.contents r)
+let inflate_string ~algorithm str =
+  match algorithm with
+  | `Deflate -> inflate_string_de str
+  | `Gzip -> inflate_string_gz str
 
 
-let compress_string ~cfg ?(level = 4) str =
-  let i = De.bigstring_create De.io_buffer_size in
-  let o = De.bigstring_create De.io_buffer_size in
-  let w = De.Lz77.make_window ~bits:15 in
-  let q = De.Queue.create 0x1000 in
-  let r = Buffer.create 0x1000 in
-  let p = ref 0 in
-  let refill buf =
-    let len = min (String.length str - !p) De.io_buffer_size in
-    Bigstringaf.blit_from_string str ~src_off:!p buf ~dst_off:0 ~len;
-    p := !p + len;
-    len
-  in
-  let flush buf len =
-    let str = Bigstringaf.substring buf ~off:0 ~len in
-    Buffer.add_string r str
-  in
-  Gz.Higher.compress ~level ~w ~q ~refill ~flush () cfg i o;
-  Buffer.contents r
+let deflate_string ~algorithm ~cfg ?level str =
+  match algorithm with
+  | `Deflate -> deflate_string_de str
+  | `Gzip -> deflate_string_gz ~cfg ?level str
--- src/cuz_cohttp.ml
+++ src/cuz_cohttp.ml
@@ -1,21 +1,45 @@
-exception GzipError of string
+exception CuzError of string
 
-let extract (resp, body) =
+let encoding_of_string = function
+  | "deflate" -> `Deflate
+  | "gzip" -> `Gzip
+  | s -> `Unknown s
+
+
+let content_encodings s =
+  String.split_on_char ',' s
+  |> List.map (fun x -> x |> String.trim |> String.lowercase_ascii)
+  |> List.map encoding_of_string
+  |> Option.some
+
+
+let decompress (resp, body) =
+  let rec aux algorithms content =
+    match algorithms with
+    | [] -> Ok content
+    | (`Deflate as el) :: rest | (`Gzip as el) :: rest ->
+      Result.bind (Cuz.inflate_string ~algorithm:el content) (aux rest)
+    | `Unknown d :: _rest -> Error (`Msg ("Unsopported encoding directive '" ^ d ^ "'"))
+  in
   let open Lwt.Syntax in
   let* body = Cohttp_lwt.Body.to_string body in
-  let is_gzipped =
-    Cohttp_lwt.Response.headers resp
-    |> fun resp -> Cohttp.Header.get resp "content-encoding" = Some "gzip"
+  let algorithms =
+    let headers = Cohttp_lwt.Response.headers resp in
+    let algorithms = Cohttp.Header.get headers "content-encoding" in
+    Option.bind algorithms content_encodings
   in
-  let body = if is_gzipped then Cuz.uncompress_string body else Ok body in
-  match body with
-  | Ok content -> Lwt.return content
-  | Error (`Msg error) -> Lwt.fail (GzipError error)
+  match algorithms with
+  | None -> Lwt.return body
+  | Some algorithms ->
+    let body = aux algorithms body in
+    (match body with
+    | Ok body -> Lwt.return body
+    | Error (`Msg err) -> Lwt.fail (CuzError err))
 
 
-let accept_gzip =
+let accept_gzde =
   let open Cohttp.Header in
-  let gzip_h = of_list [ "accept-encoding", "gzip" ] in
+  let gzip_h = of_list [ "accept-encoding", "gzip,deflate" ] in
   function
   | None -> gzip_h
-  | Some h -> add h "accept-encoding" "gzip"
+  | Some h -> add h "accept-encoding" "gzip,deflate"
